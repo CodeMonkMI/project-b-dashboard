d374d2ad14028963729d4cf6e4acfebf4f2b1482
Monirul Islam <mmislam027@gmail.com>
2024-03-28 14:31:29 +0600

    sign out feature added

--- src/layouts/SidebarLayout/Header/Userbox/index.tsx
+++ src/layouts/SidebarLayout/Header/Userbox/index.tsx
@@ -4,59 +4,27 @@ import { NavLink } from 'react-router-dom';
 
 import {
   Avatar,
-  Box,
-  Button,
   Divider,
   Hidden,
-  lighten,
   List,
   ListItem,
   ListItemText,
-  Popover,
-  Typography
+  Popover
 } from '@mui/material';
 
-import InboxTwoToneIcon from '@mui/icons-material/InboxTwoTone';
-import { styled } from '@mui/material/styles';
-import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone';
 import AccountBoxTwoToneIcon from '@mui/icons-material/AccountBoxTwoTone';
-import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone';
 import AccountTreeTwoToneIcon from '@mui/icons-material/AccountTreeTwoTone';
+import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone';
+import InboxTwoToneIcon from '@mui/icons-material/InboxTwoTone';
+import SignOut from './SignOut';
 
-const UserBoxButton = styled(Button)(
-  ({ theme }) => `
-        padding-left: ${theme.spacing(1)};
-        padding-right: ${theme.spacing(1)};
-`
-);
-
-const MenuUserBox = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.colors.alpha.black[5]};
-        padding: ${theme.spacing(2)};
-`
-);
-
-const UserBoxText = styled(Box)(
-  ({ theme }) => `
-        text-align: left;
-        padding-left: ${theme.spacing(1)};
-`
-);
-
-const UserBoxLabel = styled(Typography)(
-  ({ theme }) => `
-        font-weight: ${theme.typography.fontWeightBold};
-        color: ${theme.palette.secondary.main};
-        display: block;
-`
-);
-
-const UserBoxDescription = styled(Typography)(
-  ({ theme }) => `
-        color: ${lighten(theme.palette.secondary.main, 0.5)}
-`
-);
+import {
+  MenuUserBox,
+  UserBoxButton,
+  UserBoxDescription,
+  UserBoxLabel,
+  UserBoxText
+} from './styled';
 
 function HeaderUserbox() {
   const user = {
@@ -134,12 +102,7 @@ function HeaderUserbox() {
           </ListItem>
         </List>
         <Divider />
-        <Box sx={{ m: 1 }}>
-          <Button color="primary" fullWidth>
-            <LockOpenTwoToneIcon sx={{ mr: 1 }} />
-            Sign out
-          </Button>
-        </Box>
+        <SignOut />
       </Popover>
     </>
   );
